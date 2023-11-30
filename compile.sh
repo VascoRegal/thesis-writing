cc7a3924232249a78cf2b200d93659fddcbe6a79
VascoRegal <vascoregal24@gmail.com>
2023-11-30 16:57:03 +0000

    merge refs to bibtex

--- compile.sh
+++ compile.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-latexmk -pdf -auxdir=./build pre-tese.tex
+
+# make pdf and compile bib
+pdflatex -output-directory=build -interaction=nonstopmode pre-tese
+bibtex pre-tese
+pdflatex -output-directory=build -interaction=nonstopmode pre-tese
+pdflatex -output-directory=build -interaction=nonstopmode pre-tese
+
+
+# make pdf
+# latexmk -pdf -auxdir=./build pre-tese.tex
 
